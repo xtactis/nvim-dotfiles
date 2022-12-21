bd4d5573125882f66523e93b5a3127cd8c58ae0e
Matija Dizdar <matija.dizdar@protonmail.com>
2022-12-21 21:21:01 +0900

    actual ros support

--- lua/md/packer.lua
+++ lua/md/packer.lua
@@ -12,7 +12,45 @@ return require('packer').startup(function()
     use 'lukas-reineke/indent-blankline.nvim'
     use 'nvim-treesitter/nvim-treesitter'
     use { 'thibthib18/ros-nvim', config=function()
-        require 'ros-nvim'.setup({})
+        require 'ros-nvim'.setup({
+            catkin_ws_path = "~/catkin_ws",
+            catkin_program = "catkin build",
+            terminal_height = 8,
+            node_picker_mappings = function(map)
+                map("n", "<c-k>", vim_utils.open_terminal_with_format_cmd_entry("rosnode kill %s"))
+                map("i", "<c-k>", vim_utils.open_terminal_with_format_cmd_entry("rosnode kill %s"))
+            end,
+            topic_picker_mappings = function(map)
+                local cycle_previewers = function(prompt_bufnr)
+                    local picker = action_state.get_current_picker(prompt_bufnr)
+                    picker:cycle_previewers(1)
+                end
+                map("n", "<c-b>", vim_utils.open_terminal_with_format_cmd_entry("rostopic pub %s"))
+                map("i", "<c-b>", vim_utils.open_terminal_with_format_cmd_entry("rostopic pub %s"))
+            -- While browsing topics, press <c-e> to switch between `rostopic info` and `rostopic echo`
+                map("n", "<c-e>", cycle_previewers)
+                map("i", "<c-e>", cycle_previewers)
+            end,
+            service_picker_mappings = function(map)
+                map("n", "<c-e>", vim_utils.open_terminal_with_format_cmd_entry("rosservice call %s"))
+                map("i", "<c-e>", vim_utils.open_terminal_with_format_cmd_entry("rosservice call %s"))
+            end,
+            param_picker_mappings = function(map)
+                map("n", "<c-e>", vim_utils.open_terminal_with_format_cmd_entry("rosparam set %s"))
+                map("i", "<c-e>", vim_utils.open_terminal_with_format_cmd_entry("rosparam set %s"))
+            end
+        })
+        vim.keymap.set('n', '<leader>fp', function() require("ros-nvim.telescope.package").search_package() end, {})
+        vim.keymap.set('n', '<leader>fgp', function() require("ros-nvim.telescope.package").grep_package() end, {})
+        vim.keymap.set('n', '<leader>rtl', function() require("ros-nvim.telescope.pickers").topic_picker() end, {})
+        vim.keymap.set('n', '<leader>rnl', function() require("ros-nvim.telescope.pickers").node_picker() end, {})
+        vim.keymap.set('n', '<leader>rsl', function() require("ros-nvim.telescope.pickers").service_picker() end, {})
+        vim.keymap.set('n', '<leader>rds', function() require("ros-nvim.telescope.pickers").srv_picker() end, {})
+        vim.keymap.set('n', '<leader>rdm', function() require("ros-nvim.telescope.pickers").msg_picker() end, {})
+        vim.keymap.set('n', '<leader>rpl', function() require("ros-nvim.telescope.pickers").param_picker() end, {})
+        vim.keymap.set('n', '<leader>bb', function() require("ros-nvim.build").catkin_make() end, {})
+        vim.keymap.set('n', '<leader>bp', function() require("ros-nvim.build").catkin_make_pkg() end, {})
+        vim.keymap.set('n', '<leader>rt', function() require("ros-nvim.test").rostest() end, {})
       end
     }
     use {
